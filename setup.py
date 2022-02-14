50861b2f53bc5eda981d9457a50bf9c1204110f4
olegaobini <obiniolega@gmail.com>
2022-02-14 12:16:01 -0800

    inital commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pgbackuptool",
+    version="0.0.1",
+    author="Olega Obini",
+    author_email="obiniolega@gmail.com",
+    description="A Command Line Backup Tool Buit In Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/olegaobini/pgbackup",
+    project_urls={
+        "Bug Tracker": "https://github.com/olegaobini/pgbackup/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": ""},
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
