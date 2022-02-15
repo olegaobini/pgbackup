31b962a239ab21749e5abcccd8437655fe941ab6
olegaobini <obiniolega@gmail.com>
2022-02-15 09:48:08 -0800

    cleaned up cli

--- src/pgbackup/cli.py
+++ src/pgbackup/cli.py
@@ -1,8 +1,6 @@
 from argparse import ArgumentParser, Action
 import re
 
-POSTGRES_REGEX = '^postgres:\/\/*\/'
-
 
 class DriverAction(Action):
     def __call__(self, parser, namespace, values, option_string=None):
@@ -10,11 +8,13 @@ class DriverAction(Action):
         namespace.driver = driver.lower()
         namespace.destination = destination
 
+
 def create_parser() -> ArgumentParser:
     parser = ArgumentParser(
         description='Create backup of postgres database')
 
     parser.add_argument('url', help='the postgres url')
+
     parser.add_argument('--driver',
                         help='how & where to store backup',
                         nargs=2,
