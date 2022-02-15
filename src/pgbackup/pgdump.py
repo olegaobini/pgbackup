0a12593a8d2a1c6cc77a6f134c43b0ab2febc1b7
olegaobini <obiniolega@gmail.com>
2022-02-15 09:48:51 -0800

    created dump functionality

---
+++ src/pgbackup/pgdump.py
@@ -0,0 +1,12 @@
+import sys
+import subprocess
+
+POSTGRES_REGEX = '^postgres:\/\/*\/'
+
+
+def dump(url):
+    try:
+        return subprocess.Popen(['pg_dump', url], stdout=subprocess.PIPE)
+    except OSError as err:
+        print(f'Error: {err}')
+        sys.exit(1)
