332e7f7b1703761e27d52dfc4c836e52c115bc83
olegaobini <obiniolega@gmail.com>
2022-02-16 12:07:19 -0800

    naming functionality

--- src/pgbackup/pgdump.py
+++ src/pgbackup/pgdump.py
@@ -6,7 +6,7 @@ POSTGRES_REGEX = '^postgres:\/\/*\/'
 
 def dump(url):
     try:
-        return subprocess.Popen(['pg_dump', url], stdout=subprocess.PIPE)
+        return subprocess.Popen(['pg_dump', url], stdout=subprocess.PIPE), True
     except OSError as err:
         print(f'Error: {err}')
         sys.exit(1)
