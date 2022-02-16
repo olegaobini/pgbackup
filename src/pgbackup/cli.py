e23dfd31a8c49adb801aa3b1740d9466a5fd2de4
olegaobini <obiniolega@gmail.com>
2022-02-16 12:06:16 -0800

    naming functionality

--- src/pgbackup/cli.py
+++ src/pgbackup/cli.py
@@ -1,4 +1,8 @@
 from argparse import ArgumentParser, Action
+from pgbackup.storage import s3, local
+from pgbackup.pgdump import dump
+from io import BytesIO
+import sys
 
 
 class DriverAction(Action):
@@ -20,12 +24,44 @@ def create_parser() -> ArgumentParser:
     parser.add_argument('url', help='the postgres url')
 
     parser.add_argument('--driver',
-                        help='how & where to store backup',
+                        help='How & where to store backup',
                         nargs=2,
                         action=DriverAction,
                         required=True)
 
     parser.add_argument(
-        '--name', help='the name of the backup', nargs=1, action=NameAction, required=False)
+        '--name', help='Name of the backup', nargs=1, action=NameAction, required=False)
 
     return parser
+
+
+def main():
+    parser = create_parser()
+    args = parser.parse_args()
+    print('Attempting To Contacting Postgress Server')
+    output = dump(args.url).communicate()[0]
+    print('Successfully Concluded Contact\n')
+    backup_handler(args.driver, args.destination,
+                   output, name=args.name[0])
+
+
+def backup_handler(driver, location, dbData, name):
+    if name == None:
+        name = 'backup'
+
+    try:
+        if driver == 's3':
+            s3(BytesIO(dbData), location, f'{name}.tar')
+        elif driver == 'local':
+            with open(f'{location}/{name}.tar', 'w+b') as f:
+                print(f'Backing Up Data To: {location}/{name}.tar')
+                local(dbData, f)
+        else:
+            print(f'Invalid Driver "{driver}": (local, s3)')
+    except OSError as err:
+        print(
+            f"OS error occurred trying to open {location}{name}.tar\nError {err}")
+        sys.exit(1)
+    except Exception as err:
+        print(f'Unexpected Error: {err}')
+        sys.exit(1)
